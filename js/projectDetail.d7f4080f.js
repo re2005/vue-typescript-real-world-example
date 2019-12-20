64c20be6f4f288194dc750d5efc5eef08d2f226f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-20 09:45:37 +0000

    Deploy re2005/vue-typescript-real-world-example to github.com/re2005/vue-typescript-real-world-example.git:gh-pages

---
+++ js/projectDetail.d7f4080f.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["projectDetail"],{cbaf:function(t,e,n){"use strict";n.r(e);var a=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},c=[function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",{staticClass:"about"},[n("h1",[t._v("Project Detail")])])}],l=n("2877"),r={},s=Object(l["a"])(r,a,c,!1,null,null,null);e["default"]=s.exports}}]);
+//# sourceMappingURL=projectDetail.d7f4080f.js.map
